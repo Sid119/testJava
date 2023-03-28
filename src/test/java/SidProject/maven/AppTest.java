a25a020d97cae398e5c75c03a95f8852321836a7
Sid <Awasthi>
2023-03-28 17:01:08 +0530

    first commit

---
+++ src/test/java/SidProject/maven/AppTest.java
@@ -0,0 +1,20 @@
+package SidProject.maven;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
---
+++ src/test/java/SidProject/maven/test.java
@@ -0,0 +1,25 @@
+package SidProject.maven;
+
+
+
+public class test {
+	
+	int a = 10 , b = 20 , sum;
+	
+	void sumOfNumber() {
+		sum = a + b;
+		System.out.println(sum);
+		System.out.println("Hello Sid");
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		test sum = new test();
+		sum.sumOfNumber();
+		
+		
+		
+		
+	}
+
+}
